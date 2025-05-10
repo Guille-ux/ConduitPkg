665de2c173454cd08a86983f00fe9c5dd8a0ec90
Guille <guilleleiratemes@gmail.com>
2025-05-10 17:26:53 +0100

    making installs

--- python-package/src/conduitpkg/install/install_pkg.py
+++ python-package/src/conduitpkg/install/install_pkg.py
@@ -2,13 +2,16 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import git
+from git import Repo
 import os
 import json
 from . import search_packet as search
 from . import dependencies
+from urllib.request import urlopen, urlretrieve
+from ..packaging.extracting import extract
+import shutil
 
-def get_packet(name):
+def get_packet(name, protocol):
     print("[+] Searching Packet [+]")
     print("[+] Listing Repos [+]")
     repos_list = search.get_repos()
@@ -19,7 +22,23 @@ def get_packet(name):
         if search.is_in_repo(repo, name):
             where.append(repo)
             print(f"[+] Package {name} is in {repo} [+]")
-    if len(where) > 1:
+    if len(where) < 1:
+        print("[!] Packet doesn't exist [!]")
+    elif len(where) > 1:
         print("[!] Warning: this is i various repos [!]")
+        # añadir manejo para elegir
+        return
+    else:
+        repo = repos_list[0]
+    pkg_list = search.get_pkg_list(repo)
+    pkg_url = pkg_list[name]
     # tengo que hacer la parte de descarga del paquete
     # primero averiguar donde esta el paquete, y despues descargarlo
+    if protocol == "git":
+        print("[+] Cloning [+]")
+        Repo.clone_from(pkg_url, ".")
+        extract.gextract(name)
+        print(f"[+] Packet '{name}' Installed [+]")
+    elif protocol=="http":
+        urlretrieve(pkg_url, name+".zip")
+        extract.extract(name)
--- python-package/src/conduitpkg/packaging/extracting/extract_packet.py
+++ python-package/src/conduitpkg/packaging/extracting/extract_packet.py
@@ -8,9 +8,23 @@ import pathlib
 from zynk_lite import interpreter
 
 def extract(name):
+    current = os.chown()
     user_root = os.path.expanduser("~")
     packet_path = pathlib.Path.joinpath(user_root, ".conduitpkg", name)
     shutil.unpack_archive(name+".zip", packet_path)
     os.chdir(packet_path)
     intp = interpreter.ZynkLInterpreter()
     intp.eval_file("builder.zl")
+    os.chdir(current)
+    os.remove(name+".zip")
+
+def gextract(name):
+    current = os.chown()
+    user_root = os.path.expanduser("~")
+    packet_path = pathlib.Path.joinpath(user_root, ".conduitpkg", name)
+    shutil.copytree(name, packet_path)
+    os.chdir(packet_path)
+    intp = interpreter.ZynkLInterpreter()
+    intp.eval_file("builder.zl")
+    os.chdir(current)
+    shutil.rmtree(name)
