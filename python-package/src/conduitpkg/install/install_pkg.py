83e50922e2bd01521b2f0d47934dc9cbb13b73cb
Guille on a Raspberry pi <guilleleiratemes@gmail.com>
2025-05-10 02:29:29 +0200

    adding get packet, that isn't finished

--- python-package/src/conduitpkg/install/install_pkg.py
+++ python-package/src/conduitpkg/install/install_pkg.py
@@ -1,3 +1,23 @@
 # SPDX-FileCopyrightText: 2025-present Guille on a Raspberry pi <guilleleiratemes@gmail.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+import git
+import os
+import json
+from . import search_packet as search
+from . import dependencies
+
+def get_packet(name):
+    print("[+] Searching Packet [+]")
+    print("[+] Listing Repos [+]")
+    repos_list = search.get_repos()
+    where = []
+    print("[+] Searching on repos [+]")
+    for repo in repos_list:
+        print(f"[+] Searching on Repo {repo} the packet {name} [+]")
+        if search.is_in_repo(repo, name):
+            where.append(repo)
+            print(f"[+] Package {name} is in {repo} [+]")
+    if len(where) > 1:
+        print("[!] Warning: this is i various repos [!]")
